9edffe3f488c2c4550f156f4dec0a79f5f83f4d4
rachidcooder <cooderherou@gmail.com>
2023-12-28 16:10:30 +0000

    fix routes

--- frontend/src/RoutersApi/ApiRoutes.jsx
+++ frontend/src/RoutersApi/ApiRoutes.jsx
@@ -1,34 +1,34 @@
-const host ="http://localhost:5000/api" ;
+const host = "https://onlay-shop-api.onrender.com/api";
 
 //auth
-export const loginRoute=`${host}/user/login` ;//POST
-export const RegisterRoute=`${host}/user`;//POST
+export const loginRoute = `${host}/user/login`;//POST
+export const RegisterRoute = `${host}/user`;//POST
 
 //Product
-export const getProductRoute=`${host}/products` ;//GET
-export const getOnePrRoute=`${host}/product` ;//GET
-export const getPrsWere=`${host}/products/where` ;//GET
+export const getProductRoute = `${host}/products`;//GET
+export const getOnePrRoute = `${host}/product`;//GET
+export const getPrsWere = `${host}/products/where`;//GET
 
-export const CreateProductRoute=`${host}/product` ;//POST
-export const UpdatePrRoute=`${host}/product` ;//
+export const CreateProductRoute = `${host}/product`;//POST
+export const UpdatePrRoute = `${host}/product`;//
 
 
 //Category
-export const getCategory=`${host}/category/` ;//GET
-export const CreateCategoryRoute=`${host}/categories/` ;
-export const getOneCategory=`${host}/category/` ; //with : id
+export const getCategory = `${host}/category/`;//GET
+export const CreateCategoryRoute = `${host}/categories/`;
+export const getOneCategory = `${host}/category/`; //with : id
 //
-export const AddcardRoute=`${host}/cart/add-create` ;
-export const getcardRoute=`${host}/cart/get` ;
-export const deletFromCardRoute=`${host}/cart/delete/` ;//POST with : id
+export const AddcardRoute = `${host}/cart/add-create`;
+export const getcardRoute = `${host}/cart/get`;
+export const deletFromCardRoute = `${host}/cart/delete/`;//POST with : id
 
-export const addOrderRoute=`${host}/order/add-order/` ;//POST
-export const getOrdersRoute=`${host}/order/get-orders/` ;//GET
+export const addOrderRoute = `${host}/order/add-order/`;//POST
+export const getOrdersRoute = `${host}/order/get-orders/`;//GET
 
-export const addAddressRoute=`${host}/address/add/` ;//POST
-export const getAddressRoute=`${host}/address/get/` ;//GET
+export const addAddressRoute = `${host}/address/add/`;//POST
+export const getAddressRoute = `${host}/address/get/`;//GET
 
-export const PaymentAddRoute=`${host}/paymentcard/add-paymentcard/`;//POST
-export const PaymentGetRoute=`${host}/paymentcard/get-paymentcard/`;//GET
+export const PaymentAddRoute = `${host}/paymentcard/add-paymentcard/`;//POST
+export const PaymentGetRoute = `${host}/paymentcard/get-paymentcard/`;//GET
 
 
