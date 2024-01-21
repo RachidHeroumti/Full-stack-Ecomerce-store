f35320c7082885a13102999722cafe0aa16bfe20
rachidcooder <cooderherou@gmail.com>
2024-01-21 13:25:30 +0000

    address check

--- frontend/src/Compoanent/Address.jsx
+++ frontend/src/Compoanent/Address.jsx
@@ -1,27 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { countyCode } from '../data/data'
 import { useNavigate } from 'react-router-dom'
-import {ToastContainer,toast} from "react-toastify"
-import {EcoState} from "../Context/EcoProvider"
+import { ToastContainer, toast } from "react-toastify"
+import { EcoState } from "../Context/EcoProvider"
 import axios from 'axios'
 import { addAddressRoute } from '../RoutersApi/ApiRoutes'
 import Cookies from 'js-cookie'
 
 function Address() {
-  const{setAddress,userToken} =EcoState()
- const[countryName,setCountryName] =useState("Morocco");
-  const[countryCode,setCountryCode] =useState("+212");
- const[contactName,setContactName] =useState("");
- const[mobile,setMobile] =useState();
-  const[city,setCity] =useState("");
-  const[street,setStreet] =useState("");
-const[province,setProvince] =useState("");
-const[zip,setZip] =useState("");
-const[asdefAddress,setAsdefAdress]=useState(true) ;
+  const { setAddress, userToken } = EcoState()
+  const [countryName, setCountryName] = useState("Morocco");
+  const [countryCode, setCountryCode] = useState("+212");
+  const [contactName, setContactName] = useState("");
+  const [mobile, setMobile] = useState();
+  const [city, setCity] = useState("");
+  const [street, setStreet] = useState("");
+  const [province, setProvince] = useState("");
+  const [zip, setZip] = useState("");
+  const [asdefAddress, setAsdefAdress] = useState(true);
 
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
-     const toastOptions = {
+  const toastOptions = {
     position: "bottom-right",
     autoClose: 3000,
     pauseOnHover: true,
@@ -29,51 +29,59 @@ const[asdefAddress,setAsdefAdress]=useState(true) ;
     theme: "dark",
   };
 
+  useEffect(() => {
+    const ad = localStorage.getItem("ad");
+    if (ad) {
+      navigate('/Order');
+    }
+  })
 
-  useEffect(()=>{
-    const token =Cookies.get('token')
-     if(!userToken&&!token){
+  useEffect(() => {
+    const token = Cookies.get('token')
+    if (!userToken && !token) {
       navigate("/login");
-     }
-  },[])
+    }
+  }, [])
 
-  const onSave=async()=>{
-    if(!countryName||!contactName||!mobile||!city||!street||!province||!zip){ 
-       toast.error("some fields are empty !",toastOptions)
+  const onSave = async () => {
+    if (!countryName || !contactName || !mobile || !city || !street || !province || !zip) {
+      toast.error("some fields are empty !", toastOptions)
     }
     else {
-       const config={
-           headers:{
-            Authorization :`Bearer ${userToken}`,
-           }}
-       try{
-        const res= await axios.post(addAddressRoute,{
-          contactName,country:countryName,
-          mobile,city,street,province,zipcode:zip,asdefAddress
-        },config);
-        if(res.data){
-          setAddress(res.data.address);
-        navigate('/Order');
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
         }
-       }catch(err){
+      }
+      try {
+        const res = await axios.post(addAddressRoute, {
+          contactName, country: countryName,
+          mobile, city, street, province, zipcode: zip, asdefAddress
+        }, config);
+        if (res.data) {
+          setAddress(res.data.address);
+          localStorage.setItem("ad", "ad");
+          navigate('/Order');
+        }
+      } catch (err) {
         console.log(err);
-       }
+      }
 
     }
 
-     
-    }
 
-const setCodeCountry = (name) => {
-  let code ="";
+  }
+
+  const setCodeCountry = (name) => {
+    let code = "";
     countyCode.filter((it) => {
       if (it.name === name) {
-        code=it.code ;
+        code = it.code;
         setCountryCode(code);
       }
     })
-  
-};
+
+  };
 
 
 
@@ -83,86 +91,86 @@ const setCodeCountry = (name) => {
 
       <div className=''>
         <h1 className=' md:text-3xl text-xl font-bold py-6'>Shipping Address</h1>
-         <div className=' bg-gray-50 shadow-sm p-4'>
+        <div className=' bg-gray-50 shadow-sm p-4'>
           {/* country  */}
-           <h1 className='text-xl font-bold py-3 mt-3'>Country/Region</h1>
+          <h1 className='text-xl font-bold py-3 mt-3'>Country/Region</h1>
 
-           <select className='px-10 py-2 rounded border border-gray-400 outline-none' 
-              
-             value={countryName}
-                onChange={(e)=>{setCountryName(e.target.value) ; setCodeCountry(e.target.value)}}
-           >
+          <select className='px-10 py-2 rounded border border-gray-400 outline-none'
 
-          { countyCode.map((country,i)=>{
-            return(
-            <option key={i} value={country.name}>
-             {country.name}
-             </option>)
-           }) 
-             }
-           </select>
-         </div>
+            value={countryName}
+            onChange={(e) => { setCountryName(e.target.value); setCodeCountry(e.target.value) }}
+          >
+
+            {countyCode.map((country, i) => {
+              return (
+                <option key={i} value={country.name}>
+                  {country.name}
+                </option>)
+            })
+            }
+          </select>
+        </div>
         {/* Personal info */}
         <div className='bg-gray-50 shadow-sm p-4 mt-3'>
           <h1 className='text-xl font-bold py-3'>Personal Information</h1>
-           <div className=''>
-             <input className='px-5 py-2 outline-none rounded-lg m-2
+          <div className=''>
+            <input className='px-5 py-2 outline-none rounded-lg m-2
                 bg-gray-200' type='text' placeholder='Contact Name *'
-                value={contactName}
-                onChange={(e)=>{setContactName(e.target.value)}}
-                />
-             <div className='p-2 flex lg:flex-row flex-col'>
+              value={contactName}
+              onChange={(e) => { setContactName(e.target.value) }}
+            />
+            <div className='p-2 flex lg:flex-row flex-col'>
               <input className=' text-center py-2 outline-none rounded-lg me-2 
-                 bg-gray-200 my-1' type='text' placeholder='Country Code (+XXX)' 
-                  value={countryCode}
-                  readOnly
-                  />
+                 bg-gray-200 my-1' type='text' placeholder='Country Code (+XXX)'
+                value={countryCode}
+                readOnly
+              />
               <input className='px-5 py-2 outline-none rounded-lg  
                bg-gray-200 my-1' type='text' placeholder='Mobile Number *'
                 value={mobile}
-                onChange={(e)=>{setMobile(e.target.value)}} />
-             </div>
-           </div>
+                onChange={(e) => { setMobile(e.target.value) }} />
+            </div>
+          </div>
         </div>
         {/* Address */}
         <div className='bg-gray-50 shadow-sm p-4 mt-3  flex lg:flex-row flex-col'>
           <input className='bg-gray-200 outline-none px-5 py-2 rounded-lg m-2'
             type='text' placeholder='Street, house/apprtement *'
-             value={street}
-                onChange={(e)=>{setStreet(e.target.value)}}/>
+            value={street}
+            onChange={(e) => { setStreet(e.target.value) }} />
           <input className='bg-gray-200 outline-none px-5 py-2 rounded-lg m-2'
-           type='text' placeholder='Province*'
+            type='text' placeholder='Province*'
             value={province}
-            onChange={(e)=>{setProvince(e.target.value)}}/>
+            onChange={(e) => { setProvince(e.target.value) }} />
           <input className='bg-gray-200 outline-none px-5 py-2 rounded-lg m-2'
-             type='text' placeholder='City*'
-               value={city}
-               onChange={(e)=>{setCity(e.target.value)}}/>
+            type='text' placeholder='City*'
+            value={city}
+            onChange={(e) => { setCity(e.target.value) }} />
           <input className='bg-gray-200 outline-none px-5 py-2 rounded-lg m-2'
-             type='text' placeholder='ZIP code*'
-              value={zip}
-              onChange={(e)=>{setZip(e.target.value)}}/>
+            type='text' placeholder='ZIP code*'
+            value={zip}
+            onChange={(e) => { setZip(e.target.value) }} />
         </div>
 
         <div className='bg-gray-50 shadow-sm px-4 py-2 flex justify-center flex-col'>
           <div className='p-3 flex items-center'>
-        <label  className=' font-bold text-xl' >Set as default shipping address</label> 
-       <input
-        type="checkbox"
-        className="mx-2 w-6 h-6 text-indigo-600"
-        id="myCheckbox"
-        name="myCheckbox"
-        value={asdefAddress}
-        onChange={(e)=>{setAsdefAdress(e.target.value)}}
-      />
+            <label className=' font-bold text-xl' >Set as default shipping address</label>
+            <input
+              type="checkbox"
+              className="mx-2 w-6 h-6 text-indigo-600"
+              id="myCheckbox"
+              name="myCheckbox"
+              value={asdefAddress}
+              onChange={(e) => { setAsdefAdress(e.target.value) }}
+            />
           </div>
-      <div className='p-3  '>
-          <button  onClick={()=>{onSave()}}
-             className='rounded-lg bg-gray-400 font-bold py-1 px-5 w-full hover:bg-slate-500'>Save</button>
-        </div>
+          <div className='p-3  '>
+            <button onClick={() => { onSave() }}
+              className='rounded-lg bg-gray-400 font-bold py-1 px-5 w-full hover:bg-slate-500'>Save</button>
+          </div>
         </div>
       </div>
-      <ToastContainer/>
+      <ToastContainer />
     </div>
   )
 }
--- frontend/src/Compoanent/Order.jsx
+++ frontend/src/Compoanent/Order.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import {AiFillDelete } from 'react-icons/ai'
-import{ FaCcVisa ,FaPaypal} from 'react-icons/fa'
-import{ FaCcMastercard} from 'react-icons/fa6'
-import {ToastContainer,toast} from "react-toastify"
+import { AiFillDelete } from 'react-icons/ai'
+import { FaCcVisa, FaPaypal } from 'react-icons/fa'
+import { FaCcMastercard } from 'react-icons/fa6'
+import { ToastContainer, toast } from "react-toastify"
 import { EcoState } from '../Context/EcoProvider'
 import { useNavigate } from 'react-router-dom'
 
 
-function Order({addressInfo}) {
-  const[istoPay,setIstopPay] =useState(false) ;
-  const[tolat,setTotal] =useState(1);
- const {dataToBay,setDataToBay} =EcoState();
+function Order({ addressInfo }) {
+  const [istoPay, setIstopPay] = useState(false);
+  const [tolat, setTotal] = useState(1);
+  const { dataToBay, setDataToBay } = EcoState();
+  const [cardN, setCardN] = useState("");
+  const [cvv, setCvv] = useState("");
 
-  const navigate=useNavigate();
-     const toastOptions = {
+
+  const navigate = useNavigate();
+  const toastOptions = {
     position: "bottom-right",
     autoClose: 3000,
     pauseOnHover: true,
@@ -21,120 +24,127 @@ function Order({addressInfo}) {
     theme: "dark",
   };
 
-  const OrderNow=async()=>{
-     toast.success("order succes",toastOptions);
-      navigate("/")
+  const OrderNow = async () => {
+    console.log(cardN + "lkjf");
+    if (!cardN) {
+      toast.error("Please add payment information !", toastOptions);
+      return;
+    }
+    toast.success("order succes", toastOptions);
+    navigate("/")
   }
-  useEffect(()=>{
-    console.log("data comes to buy : ",dataToBay);
-  },[dataToBay]);
-    const addToTotal =()=>{
-              let t=0;
-            dataToBay.map((item,i)=>{
-             const price = parseFloat(item.price); 
-          if (!isNaN(price)) {
-              t += price;
-           }
-        })
-        setTotal(t) ;
-     }   
-
-  useEffect(()=>{
-      addToTotal();
-     },[]) ;
-
-     const onDeletFromOrder=(i)=>{ 
-       console.log(i);
-    const productsCopy =dataToBay;
-      const deletedItem = productsCopy.splice(i+1,0);
-       setDataToBay(
-         productsCopy.filter((item)=>{
-          return item.title!=i.title ;
-         })
-       );
+  useEffect(() => {
+    console.log("data comes to buy : ", dataToBay);
+  }, [dataToBay]);
+  const addToTotal = () => {
+    let t = 0;
+    dataToBay.map((item, i) => {
+      const price = parseFloat(item.price);
+      if (!isNaN(price)) {
+        t += price;
       }
+    })
+    setTotal(t);
+  }
+
+  useEffect(() => {
+    addToTotal();
+  }, []);
+
+  const onDeletFromOrder = (i) => {
+    console.log(i);
+    const productsCopy = dataToBay;
+    const deletedItem = productsCopy.splice(i + 1, 0);
+    setDataToBay(
+      productsCopy.filter((item) => {
+        return item.title != i.title;
+      })
+    );
+  }
 
 
   return (
     <div className='max-h-[1640] p-4 mb-12'>
-        <h1 className='text-3xl font-bold p-5 flex justify-center'>Order Confirmation</h1>
-        <div className='px-3 shadow-md py-2 my-5 bg-gray-100 rounderd-lg'>
-          <h2 className='text-2xl'>Shopping Address</h2>
-           <h3 className='text-xl text-gray-900'>{addressInfo.contactName}</h3>
-           <h4 className=' text-gray-700'>+212 {addressInfo.mobile}</h4>
-           <p className='text-gray-700'>{addressInfo.street}</p>
-        </div>
-        <div className=' bg-gray-100 m-2 p-1 rounded '>
-          <h1 className='text-xl font-bold'>Payment Methods</h1>
-          <h2 className='text-xl text-blue-500 cursor-pointer'
-           onClick={()=>{setIstopPay(true)}}
-          >Select Payment Method</h2>
-        </div>
-  <div className='grid gap-5'>
-
-      {dataToBay && dataToBay.map((item,i)=>{
-  return ( <div className='rounded bg-gray-100' key={i}>
-         <div className='flex flex-row p-4'>
-          <img className='rounded-lg h-[150px] w-[100px] '
-             src={item.image}alt='' />
-          <div className='px-3'>
-            <h1 className='text-xl font-semibold py-1'>{item.title} </h1>
-            <h1 className='text-2xl font-bold text-orange-600 py-1'>{item.price} <span>$</span></h1>
-             <div className='flex flex-row'>
-            <AiFillDelete size={25} className='mx-1' onClick={()=>{onDeletFromOrder(item)}}/>
-            <input type='number'  defaultValue={1}
-               className=' outline-none bg-gray-200 mx-2 px-2 rounded-xl text-center' min={1} max={10}/>
-          </div>
-          </div>
-         </div>
-       
-       </div>)
-      })
-     }
-      
-     
-    </div>  
-    <div className=' fixed bottom-0 left-0 bg-white w-full flex  p-5 justify-between flex-col md:flex-row'>
-      <h1 className='text-2xl font-bold px-10 w-full'>Total  :  <span className=' text-orange-600'>{tolat} $</span></h1>
-      <button className=' w-full text-center py-1 rounded-full bg-orange-500 text-white text-xl font-bold'
-         onClick={()=>{OrderNow()}}
-         >Order Now</button>
-    </div>
-    <ToastContainer/>
-    {/** To add card */}
-  {istoPay ? <div className='fixed h-screen w-full bg-black/80 top-0 left-0 z-10'>
-  </div>:""}
-{ istoPay? <div 
-  className='w-full h-screen z-10 fixed top-0 left-0  flex justify-center items-center'>
-    <div className='w-[400px] bg-gray-200 p-5 rounded-lg'>
-      <div className=' bg-white my-1 rounded-lg p-2'>
-       <h1 className=' font-bold text-center'>Add a new card</h1>
-        <div className='flex flex-row'>
-           <FaCcVisa size={20}  className='mx-2'/>
-            <FaCcMastercard size={20}  className='mx-2'/>
-             <FaPaypal  size={20} className='mx-2'/>
-        </div>
+      <h1 className='text-3xl font-bold p-5 flex justify-center'>Order Confirmation</h1>
+      <div className='px-3 shadow-md py-2 my-5 bg-gray-100 rounderd-lg'>
+        <h2 className='text-2xl'>Shopping Address</h2>
+        <h3 className='text-xl text-gray-900'>{addressInfo.contactName}</h3>
+        <h4 className=' text-gray-700'>+212 {addressInfo.mobile}</h4>
+        <p className='text-gray-700'>{addressInfo.street}</p>
       </div>
-      <input type='text' placeholder='Card Number'
-        className='w-full my-1 outline-none p-2 rounded-lg'/>
-         <input type='text' placeholder='MM/YY'
-        className='w-full my-1 outline-none p-2 rounded-lg'/>
-         <input type='text' placeholder='CVV'
-        className='w-full my-1 outline-none p-2 rounded-lg'/>
+      <div className=' bg-gray-100 m-2 p-1 rounded '>
+        <h1 className='text-xl font-bold'>Payment Methods</h1>
+        <h2 className='text-xl text-blue-500 cursor-pointer'
+          onClick={() => { setIstopPay(true) }}
+        >Select Payment Method</h2>
+      </div>
+      <div className='grid gap-5'>
 
-        <div className='flex items-center py-3'>
-        <label className='px-3'>Save card details</label>
-         <input type='checkbox'  defaultChecked
-           className='w-6 h-6 rounded-lg outline-none'/>
-       </div>
-  <button onClick={()=>{setIstopPay(false)}}
-   className="text-xl bg-orange-500 font-bold text-center py-1 rounded-full w-full text-white">
-    save $ confirm
-  </button>
+        {dataToBay && dataToBay.map((item, i) => {
+          return (<div className='rounded bg-gray-100' key={i}>
+            <div className='flex flex-row p-4'>
+              <img className='rounded-lg h-[150px] w-[100px] '
+                src={item.image} alt='' />
+              <div className='px-3'>
+                <h1 className='text-xl font-semibold py-1'>{item.title} </h1>
+                <h1 className='text-2xl font-bold text-orange-600 py-1'>{item.price} <span>$</span></h1>
+                <div className='flex flex-row'>
+                  <AiFillDelete size={25} className='mx-1' onClick={() => { onDeletFromOrder(item) }} />
+                  <input type='number' defaultValue={1}
+                    className=' outline-none bg-gray-200 mx-2 px-2 rounded-xl text-center' min={1} max={10} />
+                </div>
+              </div>
+            </div>
+
+          </div>)
+        })
+        }
+
+
+      </div>
+      <div className=' fixed bottom-0 left-0 bg-white w-full flex  p-5 justify-between flex-col md:flex-row'>
+        <h1 className='text-2xl font-bold px-10 w-full'>Total  :  <span className=' text-orange-600'>{tolat} $</span></h1>
+        <button className=' w-full text-center py-1 rounded-full bg-orange-500 text-white text-xl font-bold'
+          onClick={() => { OrderNow() }}
+        >Order Now</button>
+      </div>
+      <ToastContainer />
+      {/** To add card */}
+      {istoPay ? <div className='fixed h-screen w-full bg-black/80 top-0 left-0 z-10'>
+      </div> : ""}
+      {istoPay ? <div
+        className='w-full h-screen z-10 fixed top-0 left-0  flex justify-center items-center'>
+        <div className='w-[400px] bg-gray-200 p-5 rounded-lg'>
+          <div className=' bg-white my-1 rounded-lg p-2'>
+            <h1 className=' font-bold text-center'>Add a new card</h1>
+            <div className='flex flex-row'>
+              <FaCcVisa size={20} className='mx-2' />
+              <FaCcMastercard size={20} className='mx-2' />
+              <FaPaypal size={20} className='mx-2' />
+            </div>
+          </div>
+          <input type='text' placeholder='Card Number'
+            className='w-full my-1 outline-none p-2 rounded-lg'
+            value={cardN}
+            onChange={(e) => { setCardN(e.target.value) }} />
+          <input type='text' placeholder='MM/YY'
+            className='w-full my-1 outline-none p-2 rounded-lg' />
+          <input type='text' placeholder='CVV'
+            className='w-full my-1 outline-none p-2 rounded-lg' />
+
+          <div className='flex items-center py-3'>
+            <label className='px-3'>Save card details</label>
+            <input type='checkbox' defaultChecked
+              className='w-6 h-6 rounded-lg outline-none' />
+          </div>
+          <button onClick={() => { setIstopPay(false) }}
+            className="text-xl bg-orange-500 font-bold text-center py-1 rounded-full w-full text-white">
+            save $ confirm
+          </button>
+        </div>
+      </div> : ""}
+
     </div>
-  </div>:""}
-    
-    </div> 
   )
 }
 
