8efcd362f12004f15f27c3b9ccf6230f1464454b
rachidcooder <cooderherou@gmail.com>
2024-03-03 00:02:59 +0000

    cartItems

--- frontend/src/Compoanent/CartItems.jsx
+++ frontend/src/Compoanent/CartItems.jsx
@@ -93,31 +93,30 @@ function CardItems() {
   }
 
   return (
-    <div className='max-w-[1640px] mx-auto p-4   '>
-      <div className='max-w-[1640px] mx-auto  mt-5 space-x-1'>
-
-
+    <div className='max-w-[1640px] mx-auto p-1 '>
+      <NavBar />
+      <div className='max-w-[1640px] mx-auto pt-10  mt-12 space-y-2'>
         {dataIncart.map((product, i) => {
           return (
 
-            <div key={i} className=' flex my-1 bg-gray-100 p-2'>
+            <div key={i} className=' flex my-1 bg-emerald-700 p-2  rounded-sm '>
               <img src={product.image} alt=''
                 className='rounded-lg h-[150px] w-[100px] ' />
               <div className=' p-2'>
-                <h2 className=' text-2xl font-bold py-1 '>{product.title}</h2>
-                <h2 className=' text-2xl font-extrabold p-1 text-orange-700'> {product.price}<span> $$</span></h2>
+                <h2 className=' text-2xl font-bold py-1 text-yellow-50 '>{product.title}</h2>
+                <h2 className=' text-2xl font-extrabold p-1 text-yellow-300'> {product.price}<span> $$</span></h2>
 
                 <div className='flex text-gray-900 p-2'>
                   <button onClick={() => { OnDetailes(product) }}
-                    className='flex items-center  bg-orange-400 sm:text-xl
-                       hover:bg-orange-500 rounded-full text-center  sm:px-3 p-1  my-1'>
+                    className='flex items-center  bg-yellow-400 sm:text-xl
+                       hover:bg-yellow-500 rounded-full text-center  sm:px-3 p-1  my-1'>
                     <AiFillCaretRight size={25} className='mx-1' />
                     Details</button>
 
                   <button onClick={() => { onDeletFromCart(product) }}
                     className='flex items-center rounded-full text-center py-1
-                     hover:text-orange-500 px-2
-                     text-orange-600 my-1'>
+                     hover:text-yellow-500 px-2 
+                     text-yellow-600 my-1'>
                     <AiFillDelete size={35} className='' />
                   </button>
                 </div>
@@ -133,12 +132,15 @@ function CardItems() {
 
       </div>
 
-      <div className='p-2'>
-        <h1 className='text-xl font-semibold '>SubTotal <span> ({dataIncart.length} item)</span></h1>
-        <h2 className=' text-orange-700 text-2xl font-extrabold m-1'>{total}<span> $$</span></h2>
+      <div className='p-2 text-white'>
+        <div className=' flex space-x-3 text-xl p-3'>
+          <h1 className='text-xl font-semibold text-gray-900 '>SubTotal <span> ({dataIncart.length} item)</span></h1>
+          <h2 className=' text-orange-700 text-2xl font-extrabold '>{total}<span> $$</span></h2>
+        </div>
+
         <button onClick={() => { OnBuy() }}
-          className=' w-full text-2xl font-semibold text-center bg-orange-500 rounded-xl  px-3 py-1 
-             hover:bg-orange-600'>
+          className=' w-full text-2xl font-semibold text-center bg-emerald-800 rounded-xl  px-3 py-1 
+             hover:bg-emerald-700 '>
           Buy Now</button>
       </div>
     </div>
