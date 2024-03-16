f3f9fd754398b4e271b55d6fbf61d1b529639ac2
rachidcooder <cooderherou@gmail.com>
2024-03-16 07:25:21 +0000

    details

--- frontend/src/Compoanent/Details.jsx
+++ frontend/src/Compoanent/Details.jsx
@@ -6,6 +6,7 @@ import { ToastContainer, toast } from 'react-toastify'
 import { AddcardRoute } from '../RoutersApi/ApiRoutes'
 import { EcoState } from '../Context/EcoProvider';
 import NavBar from './NavBar';
+import { useWindowSize } from '@react-hook/window-size';
 
 function Details({ product }) {
   const [quantity, setQuantity] = useState(1);
@@ -13,6 +14,8 @@ function Details({ product }) {
   const [imagePr, setImagePr] = useState(product.image)
   const { setDataToBay, dataToBay } = EcoState()
   const { userToken } = EcoState();
+  const [screenH, screenW] = useWindowSize();
+  const [isfixed, setIsFixed] = useState(false);
   const toastOptions = {
     position: "bottom-right",
     autoClose: 500,
@@ -20,6 +23,7 @@ function Details({ product }) {
     draggable: true,
     theme: "light",
   };
+
   const AddToCart = async () => {
     const token = localStorage.getItem('token');
 
@@ -48,17 +52,11 @@ function Details({ product }) {
     setQuantity(newValue);
   };
 
-  const OnBuy = (pr) => {
-    if (!userToken) {
-      navigate('/login');
-      return true;
+  useEffect(() => {
+    if (screenH <= 815 && screenW <= 940) {
+      setIsFixed(true);
     }
-    const arr = [];
-    arr.push(pr);
-    setDataToBay(arr);
-    console.log(dataToBay);
-    navigate("/order");
-  };
+  }, []);
 
 
 
@@ -67,26 +65,26 @@ function Details({ product }) {
       <NavBar />
       <div className=' flex bg-gray-50  p-4'>
         <div className='md:flex'>
-          <div className='lg:w-[500px] flex   flex-col w-full items-center sm:items-start'>
+          <div className='lg:w-[500px] flex   flex-col w-full sm:w-full items-center sm:items-start'>
             <img src={imagePr} alt='img'
-              className='max-w-[350px] sm:h-[200px]  md:h-[400px] h-[150px] object-cover' />
+              className='max-w-[350px] sm:h-[200px]  md:h-[400px] w-full h-[100px] object-cover' />
             <div className='grid grid-cols-4 gap-2 max-w-[350px]'>
               <img src={product.images[0]} alt=''
                 onClick={() => { setImagePr(product.images[0]) }}
-                className=' h-[80px] w-[60px]  sm:h-[125px] sm:w-[100px] px-1' />
+                className=' h-[60px] w-[50px]  sm:h-[125px] sm:w-[100px] ' />
               <img src={product.images[1]} alt=''
                 onClick={() => { setImagePr(product.images[1]) }}
-                className='h-[125px] w-[100px] px-1' />
+                className='h-[60px] w-[50px]   sm:h-[125px] sm:w-[100px] ' />
               <img src={product.images[2]} alt=''
                 onClick={() => { setImagePr(product.images[2]) }}
-                className='h-[125px] w-[100px] px-1' />
+                className='h-[60px] w-[50px]   sm:h-[125px] sm:w-[100px]' />
               <img src={product.images[3]} alt=''
                 onClick={() => { setImagePr(product.images[3]) }}
-                className='h-[125px] w-[100px] px-1' />
+                className='h-[60px] w-[50px]   sm:h-[125px] sm:w-[100px] ' />
             </div>
           </div>
 
-          <div className='w-full p-4 sm:flex'>
+          <div className='w-full p-4 sm:flex '>
             <div className=''>
               <h2 className=' sm:text-2xl font-bold text-gray-950 pb-2 px-2 '>{product.title}</h2>
               <h1 className=' sm:text-2xl p-1 text-orange-800 font-bold px-5'>{product.price} <span>$$</span></h1>
@@ -115,26 +113,29 @@ function Details({ product }) {
           </div>
         </div>
 
-        <div className='w-[30%] flex flex-col p-2'>
-          <h1 className=' md:text-xl text-gray-700 p-1'>Quantity </h1>
-          <input type="number"
-            className="outline-none bg-gray-100 rounded text-center sm:text-xl my-1
-                    font-bold py-2 sm:px-3" max={10} min={1}
-            value={quantity}
-            onChange={handleQuantityChange}
-          />
 
-          <h1 className=' sm:text-xl my-2'>-49 Pieces available</h1>
-          <button onClick={() => { OnBuy(product) }}
-            className='  bg-red-500 hover:bg-red-600 rounded-lg 
-             text-center  py-2 text-white  sm:text-xl sm:font-semibold my-1'>
-            Buy Now</button>
+        <div className={`w-full sm:w-[30%] ${isfixed ? "fixed bottom-0 start-0 end-0 p-3 first-line:" : ""} 
+          flex flex-col space-x-1 bg-red-100 space-y-1`}>
+          <div className=' flex flex-row sm:flex-col justify-between items-center' >
+            <h1 className=' md:text-xl text-gray-700 sm:p-1'>Quantity </h1>
+            <input type="number"
+              className="outline-none bg-gray-100 rounded text-center sm:text-xl my-1
+                    font-bold sm:py-2 sm:px-3" max={10} min={1}
+              value={quantity}
+              onChange={handleQuantityChange}
+            />
+
+            <h1 className=' sm:text-xl  my-2'>- 49 Pieces available</h1>
+          </div>
+
 
           <button onClick={() => { AddToCart() }}
-            className='flex items-center justify-center text-3xl my-1 border border-red-500
+            className='flex items-center justify-center text-xl sm:text-3xl my-1 border border-red-500
                      font-bold bg-white hover:text-red-500  text-red-700 rounded-lg text-center  '>
             <BsFillCartFill className='mx-1 ' />
             +</button>
+
+
 
         </div>
 
