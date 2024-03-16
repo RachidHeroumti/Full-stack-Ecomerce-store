e00c010afaa90d26ad663d89281ebfb858519886
rachidcooder <cooderherou@gmail.com>
2024-03-16 07:39:55 +0000

    cartItems color

--- frontend/src/Compoanent/CartItems.jsx
+++ frontend/src/Compoanent/CartItems.jsx
@@ -104,19 +104,19 @@ function CardItems() {
                 className='rounded-lg h-[150px] w-[100px] ' />
               <div className=' p-2'>
                 <h2 className=' text-2xl font-bold py-1 text-gry-950 '>{product.title}</h2>
-                <h2 className=' text-2xl font-extrabold p-1 text-orange-700'> {product.price}<span> $$</span></h2>
+                <h2 className=' text-2xl font-extrabold p-1 text-red-700'> {product.price}<span> $$</span></h2>
 
                 <div className='flex text-white p-2'>
                   <button onClick={() => { OnDetailes(product) }}
-                    className='flex items-center  bg-red-500 sm:text-xl
-                       hover:bg-red-600 rounded-xl text-center  sm:px-3 p-1  my-1'>
+                    className='flex items-center  bg-red-400 sm:text-xl
+                       hover:bg-red-500 rounded-xl text-center  sm:px-3 p-1  my-1'>
                     <AiFillCaretRight size={25} className='mx-1' />
                     Details</button>
 
                   <button onClick={() => { onDeletFromCart(product) }}
                     className='flex items-center rounded-full text-center py-1
-                     hover:text-red-500 px-2 
-                     text-red-600 my-1'>
+                     hover:text-red-400 px-2 
+                     text-red-500 my-1'>
                     <AiFillDelete size={35} className='' />
                   </button>
                 </div>
@@ -135,12 +135,12 @@ function CardItems() {
       <div className='p-2 text-white sm:flex'>
         <div className=' flex space-x-3 text-xl p-3 w-full'>
           <h1 className='text-xl font-semibold text-gray-900 '>SubTotal <span> ({dataIncart.length} item)</span></h1>
-          <h2 className=' text-orange-700 text-2xl font-extrabold '>{total}<span> $$</span></h2>
+          <h2 className=' text-red-700 text-2xl font-extrabold '>{total}<span> $$</span></h2>
         </div>
 
         <button onClick={() => { OnBuy() }}
-          className=' w-full text-2xl font-semibold text-center bg-red-700 rounded-xl  px-3 py-1 
-             hover:bg-red-600 '>
+          className=' w-full text-2xl font-semibold text-center bg-red-600 rounded-xl  px-3 py-1 
+             hover:bg-red-500 '>
           Buy Now</button>
       </div>
     </div>
