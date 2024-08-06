d340884a76bf632580849345e467fd566871cfdd
rachidcooder <cooderherou@gmail.com>
2024-08-06 15:15:51 +0100

    start implementing design patterns

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,18 +1,21 @@
 import mongoose from "mongoose";
 
+let conn = null;
 
- const connectDB=async()=>{
-
-
-  try{
-    const conn = await mongoose.connect(process.env.URL_DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-   console.log("Database connected ...!");
-  }catch(err){
-    console.log(err)
+const connectDB = async () => {
+  try {
+    if (conn === null) {
+      conn = await mongoose.connect(process.env.URL_DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Database connected ...!");
+    }
+    return conn;
+  } catch (err) {
+    console.log(err);
+    throw new Error('Database connection failed');
   }
- }
+};
 
- export default connectDB ;
+export default connectDB;
